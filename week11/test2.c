7e8ede655bbfb1386bccb9a8ecd6b9f514f97a1e
yangyi3314 <939908750@qq.com>
2019-11-17 22:32:31 -0800

    11

---
+++ week11/test2.c
@@ -0,0 +1,25 @@
+#include"my.h"
+int main()
+{
+    int pid;
+    if((pid=fork())<0)
+    {
+        perror("failed to fork@\n");
+        return -1;
+    }
+    else if(pid==0)
+    {
+        printf("%d:child is running now!\n",getpid());
+        while(1);
+        printf("%d:child is exit now!\n",getpid());
+        exit(0);
+
+    }
+    else
+    {
+        printf("%d:parent is waiting zombie now\n",getpid());
+        sleep(10);
+        printf("%d:parent is exiting now!\n");
+    }
+    exit(0);
+}
